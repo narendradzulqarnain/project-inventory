346d712292dd774c56cb8ddc75132a111422ffb1
narendradzulqarnain <narendra.dz1107@gmail.com>
2023-09-13 09:47:54 +0700

    small changes

--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'appname' : 'what do we need to survive?',
+        'appname' : 'this is an inventory',
         'name': 'Narendra Dzulqarnain',
         'class': 'PBP F'
     }
